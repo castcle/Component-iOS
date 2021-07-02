bd1b35e82d397d1a825fcbb9749454dd4c474d56
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-07-02 18:21:17 +0700

    Init Component module.

---
+++ Component/Component.h
@@ -0,0 +1,18 @@
+//
+//  Component.h
+//  Component
+//
+//  Created by Tanakorn Phoochaliaw on 2/7/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Component.
+FOUNDATION_EXPORT double ComponentVersionNumber;
+
+//! Project version string for Component.
+FOUNDATION_EXPORT const unsigned char ComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Component/PublicHeader.h>
+
+
