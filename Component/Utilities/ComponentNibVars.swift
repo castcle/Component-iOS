c573e852b43237a494cae5e3f0993a0b38976786
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-07-15 16:05:36 +0700

    Add utilities

---
+++ Component/Utilities/ComponentNibVars.swift
@@ -0,0 +1,24 @@
+//
+//  ComponentNibVars.swift
+//  Component
+//
+//  Created by Tanakorn Phoochaliaw on 15/7/2564 BE.
+//
+
+public struct ComponentNibVars {
+    // MARK: - View Controller
+    public struct ViewController {
+    }
+    
+    // MARK: - View
+    public struct Storyboard {
+    }
+    
+    // MARK: - TableViewCell
+    public struct TableViewCell {
+    }
+    
+    // MARK: - CollectionViewCell
+    public struct CollectionViewCell {
+    }
+}
