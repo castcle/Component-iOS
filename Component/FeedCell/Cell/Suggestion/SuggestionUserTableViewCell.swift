99bf0cb24d6794f97b5ac1a2b480c0a59690998b
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2022-01-23 22:55:58 +0700

    Handle follow and view profile

--- Component/FeedCell/Cell/Suggestion/SuggestionUserTableViewCell.swift
+++ Component/FeedCell/Cell/Suggestion/SuggestionUserTableViewCell.swift
@@ -32,6 +32,8 @@ import Networking
 
 public protocol SuggestionUserTableViewCellDelegate {
     func didSeeMore(_ suggestionUserTableViewCell: SuggestionUserTableViewCell, user: [Author])
+    func didTabProfile(_ suggestionUserTableViewCell: SuggestionUserTableViewCell, user: Author)
+    func didAuthen(_ suggestionUserTableViewCell: SuggestionUserTableViewCell)
 }
 
 public class SuggestionUserTableViewCell: UITableViewCell {
@@ -57,11 +59,24 @@ public class SuggestionUserTableViewCell: UITableViewCell {
     @IBOutlet weak var secondUserVerifyImage: UIImageView!
     
     public var delegate: SuggestionUserTableViewCellDelegate?
+    private var userRepository: UserRepository = UserRepositoryImpl()
     private var user: [Author] = []
+    private var isMock: Bool = true
+    private var isFollowFirst: Bool = false
+    private var isFollowSecond: Bool = false
+    let tokenHelper: TokenHelper = TokenHelper()
+    private var stage: Stage = .none
+    private var userRequest: UserRequest = UserRequest()
+    
+    enum Stage {
+        case followUser
+        case unfollowUser
+        case none
+    }
     
     public override func awakeFromNib() {
         super.awakeFromNib()
-        
+        self.tokenHelper.delegate = self
         self.titleLabel.font = UIFont.asset(.regular, fontSize: .h4)
         self.titleLabel.textColor = UIColor.Asset.white
         self.showMoreButton.titleLabel?.font = UIFont.asset(.regular, fontSize: .overline)
@@ -99,15 +114,10 @@ public class SuggestionUserTableViewCell: UITableViewCell {
     }
     
     public func configCell(user: [Author], isMock: Bool) {
+        self.isMock = isMock
+        self.updateFirstUserFollow()
+        self.updateSecondUserFollow()
         if isMock {
-            self.fitstUserFollowButton.setTitle("Follow", for: .normal)
-            self.fitstUserFollowButton.setTitleColor(UIColor.Asset.lightBlue, for: .normal)
-            self.fitstUserFollowButton.capsule(color: .clear, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
-            
-            self.secondUserFollowButton.setTitle("Following", for: .normal)
-            self.secondUserFollowButton.setTitleColor(UIColor.Asset.white, for: .normal)
-            self.secondUserFollowButton.capsule(color: UIColor.Asset.lightBlue, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
-            
             let url = URL(string: UserManager.shared.avatar)
             self.firstUserAvatarImage.kf.setImage(with: url, placeholder: UIImage.Asset.userPlaceholder, options: [.transition(.fade(0.35))])
             self.secondUserAvatarImage.kf.setImage(with: url, placeholder: UIImage.Asset.userPlaceholder, options: [.transition(.fade(0.35))])
@@ -124,19 +134,143 @@ public class SuggestionUserTableViewCell: UITableViewCell {
         }
     }
     
+    private func updateFirstUserFollow() {
+        if !self.isMock && self.user.count > 0 {
+            let user = self.user[0]
+            if user.followed {
+                self.fitstUserFollowButton.setTitle("Following", for: .normal)
+                self.fitstUserFollowButton.setTitleColor(UIColor.Asset.white, for: .normal)
+                self.fitstUserFollowButton.capsule(color: UIColor.Asset.lightBlue, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
+            } else {
+                self.fitstUserFollowButton.setTitle("Follow", for: .normal)
+                self.fitstUserFollowButton.setTitleColor(UIColor.Asset.lightBlue, for: .normal)
+                self.fitstUserFollowButton.capsule(color: .clear, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
+            }
+        } else {
+            if self.isFollowFirst {
+                self.fitstUserFollowButton.setTitle("Following", for: .normal)
+                self.fitstUserFollowButton.setTitleColor(UIColor.Asset.white, for: .normal)
+                self.fitstUserFollowButton.capsule(color: UIColor.Asset.lightBlue, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
+            } else {
+                self.fitstUserFollowButton.setTitle("Follow", for: .normal)
+                self.fitstUserFollowButton.setTitleColor(UIColor.Asset.lightBlue, for: .normal)
+                self.fitstUserFollowButton.capsule(color: .clear, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
+            }
+        }
+    }
+    
+    private func updateSecondUserFollow() {
+        if !self.isMock && self.user.count > 1 {
+            let user = self.user[1]
+            if user.followed {
+                self.secondUserFollowButton.setTitle("Following", for: .normal)
+                self.secondUserFollowButton.setTitleColor(UIColor.Asset.white, for: .normal)
+                self.secondUserFollowButton.capsule(color: UIColor.Asset.lightBlue, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
+            } else {
+                self.secondUserFollowButton.setTitle("Follow", for: .normal)
+                self.secondUserFollowButton.setTitleColor(UIColor.Asset.lightBlue, for: .normal)
+                self.secondUserFollowButton.capsule(color: .clear, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
+            }
+        } else {
+            if self.isFollowSecond {
+                self.secondUserFollowButton.setTitle("Following", for: .normal)
+                self.secondUserFollowButton.setTitleColor(UIColor.Asset.white, for: .normal)
+                self.secondUserFollowButton.capsule(color: UIColor.Asset.lightBlue, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
+            } else {
+                self.secondUserFollowButton.setTitle("Follow", for: .normal)
+                self.secondUserFollowButton.setTitleColor(UIColor.Asset.lightBlue, for: .normal)
+                self.secondUserFollowButton.capsule(color: .clear, borderWidth: 1.0, borderColor: UIColor.Asset.lightBlue)
+            }
+        }
+    }
+    
+    private func followUser() {
+        self.stage = .followUser
+        let userId: String = UserManager.shared.rawCastcleId
+        self.userRepository.follow(userId: userId, userRequest: self.userRequest) { (success, response, isRefreshToken) in
+            if !success {
+                if isRefreshToken {
+                    self.tokenHelper.refreshToken()
+                }
+            }
+        }
+    }
+    
+    private func unfollowUser() {
+        self.stage = .unfollowUser
+        let userId: String = UserManager.shared.rawCastcleId
+        self.userRepository.unfollow(userId: userId, userRequest: self.userRequest) { (success, response, isRefreshToken) in
+            if !success {
+                if isRefreshToken {
+                    self.tokenHelper.refreshToken()
+                }
+            }
+        }
+    }
+    
     @IBAction func firstUserFollowAction(_ sender: Any) {
+        if !self.isMock && self.user.count > 0 {
+            if UserManager.shared.isLogin {
+                self.userRequest.targetCastcleId = self.user[0].castcleId
+                if self.user[0].followed {
+                    self.unfollowUser()
+                } else {
+                    self.followUser()
+                }
+                self.user[0].followed.toggle()
+                self.updateFirstUserFollow()
+            } else {
+                self.delegate?.didAuthen(self)
+            }
+        } else {
+            self.isFollowFirst.toggle()
+            self.updateFirstUserFollow()
+        }
     }
     
     @IBAction func firstUserProfileAction(_ sender: Any) {
+        if !self.isMock && self.user.count > 0 {
+            self.delegate?.didTabProfile(self, user: self.user[0])
+        }
     }
     
     @IBAction func secondUserFollowAction(_ sender: Any) {
+        if !self.isMock && self.user.count > 1 {
+            if UserManager.shared.isLogin {
+                self.userRequest.targetCastcleId = self.user[1].castcleId
+                if self.user[1].followed {
+                    self.unfollowUser()
+                } else {
+                    self.followUser()
+                }
+                self.user[1].followed.toggle()
+                self.updateFirstUserFollow()
+            } else {
+                self.delegate?.didAuthen(self)
+            }
+        } else {
+            self.isFollowSecond.toggle()
+            self.updateSecondUserFollow()
+        }
     }
     
     @IBAction func secondUserProfileAction(_ sender: Any) {
+        if !self.isMock && self.user.count > 1 {
+            self.delegate?.didTabProfile(self, user: self.user[1])
+        }
     }
     
     @IBAction func showMoreAction(_ sender: Any) {
         self.delegate?.didSeeMore(self, user: self.user)
+    }
+}
+
+extension SuggestionUserTableViewCell: TokenHelperDelegate {
+    public func didRefreshTokenFinish() {
+        if self.stage == .followUser {
+            self.followUser()
+        } else if self.stage == .unfollowUser {
+            self.unfollowUser()
+        }
     }
 }
