252f74dc5d7825f1667296791a7b384bd8325fcb
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2022-02-26 22:55:17 +0700

    Fix unfollow api

--- Component/FeedCell/Cell/HeaderTableViewCell.swift
+++ Component/FeedCell/Cell/HeaderTableViewCell.swift
@@ -294,7 +294,7 @@ public class HeaderTableViewCell: UITableViewCell {
                 NotificationCenter.default.post(name: .feedReloadContent, object: nil)
             }
             
-            self.userRepository.unfollow(userId: userId, userRequest: self.userRequest) { (success, response, isRefreshToken) in
+            self.userRepository.unfollow(userId: userId, targetCastcleId: self.userRequest.targetCastcleId) { (success, response, isRefreshToken) in
                 if !success {
                     if isRefreshToken {
                         self.tokenHelper.refreshToken()
