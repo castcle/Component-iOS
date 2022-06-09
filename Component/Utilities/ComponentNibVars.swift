237a163d9a376f10a5bb1e516bf04dc8fdeb0b50
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2022-06-09 14:11:55 +0700

    Add skeleton view

--- Component/Utilities/ComponentNibVars.swift
+++ Component/Utilities/ComponentNibVars.swift
@@ -79,6 +79,7 @@ public struct ComponentNibVars {
         public static let skeleton = "SkeletonFeedTableViewCell"
         public static let skeletonUser = "SkeletonUserTableViewCell"
         public static let skeletonNotify = "SkeletonNotifyTableViewCell"
+        public static let skeletonNormal = "SkeletonNormalTableViewCell"
         public static let activityHeader = "ActivityHeaderTableViewCell"
         public static let quoteText = "QuoteCastTextCell"
         public static let quoteLink = "QuoteCastTextLinkCell"
